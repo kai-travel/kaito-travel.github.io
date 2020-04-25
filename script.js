161c275a70c9b98ea6d4f8ac2ea9a0bca4818885
kai-travel <63687928+kai-travel@users.noreply.github.com>
2020-04-25 10:18:14 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('.index-btn').click(function() {
+    $('.active').removeClass('active');
+
+
+    var clickedIndex=$('.index-btn').index($(this));
+
+  
+    $('.slide').eq(clickedIndex).addClass('active');
+
+  });
+});
