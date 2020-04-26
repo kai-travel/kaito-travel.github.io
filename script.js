65c7d88fd0318a2345cbf908469239de50c5e1a3
kai-travel <63687928+kai-travel@users.noreply.github.com>
2020-04-26 22:01:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,17 @@ $(function() {
 
   $('.text-contents').hide().fadeIn(3500);
 
+  $('.faq-list-item').click(function(){
+ var $answer = $(this).find('.answer');
+ if($answer.hasClass('open')){
+  $answer.removeClass('open') ;
+  $answer.slideUp();
+  $(this).find('span').text('+');
+ }else{
+  $answer.addClass('open') ;
+  $answer.slideDown();
+  $(this).find('span').text('-');
+ }
+});
+
 });
